f4dbc4cc2f2e56675db89cd306313b4527a40c12
Shawn Freyssonnet-Inder <shawn@vigour.io>
2014-09-04 20:26:30 +0200

    - Prevent concurrent calls to cordova.exec - Automatically initialize store when necessary

--- plugins/io.vigour.store/www/vigour-io-store.js
+++ plugins/io.vigour.store/www/vigour-io-store.js
@@ -1,55 +1,132 @@
 "use strict";
 
-var Store = module.exports;
-var PLUGIN_ID = "VigourIoStore";
+var Store = {}
+	, queue = []
+	, ready = false
+	, initCalled = false
+	, initError = false
 
-Store.StoreType = {
-	"APP_STORE": 0,
-	"PLAY_STORE": 1,
-	"MOCK_STORE": 2,
-	"AMAZON_STORE": 3
-};
+module.exports = exports = Store
 
+Store.PLUGIN_ID = 'VigourIoStore'
 
-Store.init = function(callback) {
-	cordova.exec(callback, callback, "VigourIoStore", "setup", []);
+Store.StoreTypeNames = {
+	0: 'APP_STORE'
+	, 1: 'PLAY_STORE'
+	, 2: 'MOCK_STORE'
+	, 3: 'AMAZON_STORE'
 }
 
-Store.getType = function(callback) {
-	cordova.exec(callback, callback, PLUGIN_ID, "getType", []);
+Store.getType = function(cb) {
+	exec({
+		fn: 'getType'
+		, cb: cb
+	})
 }
 
-Store.fetch = function(productIds, callback) {
-	cordova.exec(callback, callback, PLUGIN_ID, "fetch", productIds);
-};
+Store.fetch = function(productIds, cb) {
+	exec({
+		fn: 'fetch'
+		, args: [productIds]
+		, cb: cb
+	})
+}
 
-Store.buy = function(productId, callback) {
-	var productIds = [];
-	if (!(productId instanceof Array)) {
-		productIds.push(productId);
+Store.buy = function(productId, cb) {
+	exec({
+		fn: 'buy'
+		, args: [productId]
+		, cb: cb
+	})
+}
+
+Store.restore = function(cb) {
+	exec({
+		fn: 'restore'
+		, cb: cb
+	})
+}
+
+Store.unsubscribe = function (productId, cb) {
+	exec({
+		fn: 'unsubscribe'
+		, args: [productId]
+		, cb: cb
+	})
+}
+
+function exec (opts) {
+	var needsInit = (opts.needsInit === undefined) ? true : opts.needsInit
+	if (needsInit && !ready) {
+		queue.push(opts)
+		if (!initCalled) {
+			initCalled = true
+			execute({
+				fn: 'setup'
+				, cb: function (err) {
+					if (err) {
+						err.info = "This action requires store initialization, but store initialization fails, producing this error."
+						initError = err
+					} else {
+						initError = false
+						ready = true
+					}
+					next()
+				}
+			})
+		}
 	} else {
-		productIds = productId;
+		if (queue.length === 0) {
+			execute(opts)
+		} else {
+			queue.push(opts)
+		}
 	}
-	cordova.exec(callback, callback, PLUGIN_ID, "buy", productIds);
 }
 
-Store.restore = function(callback) {
-	cordova.exec(callback, callback, PLUGIN_ID, "restore", []);
+function execute (opts) {
+	var args = (opts.args === undefined) ? [] : opts.args
+	cordova.exec(
+		function (response) {
+			opts.cb(null, response)
+			next()
+		}
+		, function (err) {
+			opts.cb(err)
+			next()
+		}
+		, Store.PLUGIN_ID
+		, opts.fn
+		, args)
 }
 
+function next () {
+	var next = queue.shift()
+	if (next) {
+		if (initError) {
+			next.cb(initError)
+		} else {
+			execute(next)
+		}
+	}
+}
+
+// The following will be removed as soon as another solution is implemented natively
 Store.updatedTransactionCallback = function(state, transactionIdentifier, productId, error) {
 	switch (state) {
-		case "PaymentTransactionStatePurchased":
-			alert('purchased: ' + transactionIdentifier + ' ' + productId);
-			return;
-		case "PaymentTransactionStateFailed":
-			alert(error.description + ' ' + error.code);
-			return;
-		case "PaymentTransactionStateRestored":
-			alert('resored ' + transactionIdentifier + ' ' + productId);
-			return;
-		case "PaymentTransactionStateFinished":
-			alert('finished ' + transactionIdentifier + ' ' + productId);
-			return;
+		case 'PaymentTransactionStatePurchased':
+			alert("purchased: " + transactionIdentifier + " " + productId)
+			break
+		case 'PaymentTransactionStateFailed':
+			alert(error.description + " " + error.code)
+			break
+		case 'PaymentTransactionStateRestored':
+			alert("restored " + transactionIdentifier + " " + productId)
+			break
+		case 'PaymentTransactionStateFinished':
+			alert("finished " + transactionIdentifier + " " + productId)
+			break
+		default:
+			alert("unhandled updatedTransactionCallback state: " + state)
 	}
-};
+}
